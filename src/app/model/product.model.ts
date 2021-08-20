2d7d351e11d7b353eaa464de8f0bb56bf475c856
Carlos Ebbers <cel.ebbers@gmail.com>
2021-08-20 05:34:26 -0300

    Classe Product Repository

--- src/app/model/product.model.ts
+++ src/app/model/product.model.ts
@@ -2,7 +2,7 @@ export class Product {
     constructor(
         public id?: number,
         public name?: string,
-        public categories?: string,
+        public category?: string,
         public description?: string,
         public price?: number
     ) { }
