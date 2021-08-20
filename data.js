b96b973aa7ee0cde81d4949cdddcd3f1b753cc4c
Carlos Ebbers <cel.ebbers@gmail.com>
2021-08-19 21:02:36 -0300

    criacao de data.js, authMiddleware, index.html e configs basicas no angular.json e package.json

---
+++ data.js
@@ -0,0 +1,16 @@
+module.exports = function() {
+    return {
+        products: [
+            { id: 1, name: "O Jogo da Amarelinha", category: "Literatura", description: "Escrito por Júlio Cortazar", price: 275 },
+            { id: 2, name: "O Filosófo e o Imperador", category: "Literatura", description: "Escrito por Annabel Lyon", price: 48.95 },
+            { id: 3, name: "Viagem à Paris", category: "Viagens", description: "Escrito por Florence", price:19.50 },
+            { id: 4, name: "Londres e suas Belezas", category: "Viagens", description: "Escrito por Georgia", price: 34.95 },
+            { id: 5, name: "Navegando nos rios da Itália", category: "Viagens", description: "Escrito por Charles Lima", price: 795 },
+            { id: 6, name: "Crítica da Razão Prática", category: "Filosofia", description: "Escrito por Kant", price: 16 },
+            { id: 7, name: "A Genealogia da Moral", category: "Filosofia", description: "Escrito por Nietzsche", price: 29.95 },
+            { id: 8, name: "Assim Falou Zaratustra", category: "Filosofia", description: "Escrito por Nietzsche", price: 75} ,
+            { id: 9, name: "O Mundo de Sofia", category: "Filosofia", description: "Escrito por Jostein Gaarder", price: 120 },
+        ],
+        orders: []
+    }
+}
